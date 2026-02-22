1281007e08f82ad7cf63325f0e59a25e8e5adcef
Ben <bzmeyer18@gmail.com>
2026-02-22 10:11:26 -0500

    added print statements to test executable

--- esp_camera_test.py
+++ esp_camera_test.py
@@ -2,14 +2,17 @@ import cv2
 
 url = "http://10.42.0.124/"
 cap = cv2.VideoCapture(url)
-
+print('starting capture')
 while True:
     ret, frame = cap.read()
     if not ret:
+        print('failure')
         break
+    print('showing stream')
     cv2.imshow("ESP32 Stream", frame)
     if cv2.waitKey(1) == 27:
         break
 
 cap.release()
 cv2.destroyAllWindows
+print('done')
