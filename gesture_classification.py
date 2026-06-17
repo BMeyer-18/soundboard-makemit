f2828bc2549f978c5589f273be46ab6080c321f1
ChritiKalHex <ckone@olin.edu>
2026-06-16 23:56:47 -0400

    updating files

--- gesture_classification.py
+++ gesture_classification.py
@@ -5,11 +5,11 @@ Importing necessary modules:
 - time: used to track the duration of the video capture
 """
 
+from play_audio import PlayAudio
 import mediapipe as mp
 from mediapipe.tasks import python
 from mediapipe.tasks.python import vision
 import cv2
-from play_audio import PlayAudio
 import time
 
 class GestureClassifier():
@@ -24,16 +24,6 @@ class GestureClassifier():
         self.VisionRunningMode = mp.tasks.vision.RunningMode
         
     def classify_image(self, cam, recognizer):
-        NAME_MAP = {
-            "Thumb_Up":    "thumbs_up",
-            "Thumb_Down":  "thumbs_down",
-            "Victory":     "peace",
-            "Closed_Fist": "fist",
-            "ILoveYou":   "iluvyou",   # swap for now
-        }
-        category = NAME_MAP.get(classification[0][0].category_name)
-        if category:
-            player.play_sound(category)
         player = PlayAudio()
         ret, frame = cam.read()
         if ret:
--- play_audio.py
+++ play_audio.py
@@ -3,20 +3,33 @@ Importing necessary modules:
 - time: for sleep functionality
 - vlc: for playing audio files
 """
-
-from time import sleep
 from vlc import MediaPlayer
+from time import sleep
 
-class PlayAudio():
+class PlayAudio:
 
     def __init__(self):
-        self.happy = ("sound_files/thumb_up_yippee.mp3", 3)
-        self.sad = ("sound_files/thumb_down_downer_noise.mp3", 5)
-        self.peace = ("sound_files/victory_sign_mario_kart_win.mp3", 6)
-        self.love = ("sound_files/duck_mac-quack.mp3", 2)
-        self.mad = ("sound_files/duck_quack_reverb.mp3", 4)
-    
+        # INSTRUCTIONS (part 1):
+        # Add an instance variable for each gesture you want to include.
+        # Each variable should be a tuple in which the first element is
+        # the path to the sound effect that gesture corresponds to, and
+        # the second element is the approximate length of the sound
+        # effect in seconds (often rounded up).
+        self.happy = ("sound_files/thumb_up_yippee.mp3", 2)
+        self.sad = ("sound_files/thumb_down_downer_noise.mp3", 4)
+        self.peace = ("sound_files/victory_sign_mario_kart_win.mp3", 5)
+        self.okay = ("sound_files/duck_mac-quack.mp3", 2)
+        self.mad = ("sound_files/duck_quack_reverb.mp3", 3)
+        self.love = ("sound_files/i-love-you.mp3", 2)
+
     def play_sound(self, emotion):
+        # INSTRUCTIONS (part 2):
+        # Add a case to this match statement for each hand gesture you
+        # want the model to recognize. Each case should be the name of
+        # the hand gesture, which is the name of the folder in which the
+        # images of that hand gesture are stored.
+        # For each case, set the sound variable equal to the instance
+        # variable you defined above for the corresponding sound effect.
         match emotion:
             case "thumbs_up":
                 sound = self.happy
@@ -24,11 +37,13 @@ class PlayAudio():
                 sound = self.sad
             case "peace":
                 sound = self.peace
-            case "iluvyou":
-                sound = self.love
+            case "okay":
+                sound = self.okay
             case "fist":
                 sound = self.mad
-        
+            case "love":
+                sound = self.love
+        # You shouldn't need to modify this code. Enjoy!
         player = MediaPlayer(sound[0])
         player.play()
         sleep(sound[1])
