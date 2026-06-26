db6780afa5d957bf894eab4d38d898e4350383ac
ChritiKalHex <ckone@olin.edu>
2026-06-26 01:14:19 -0400

    created a file to download Pi only dependencies

--- seeing_soundboard_main.py
+++ seeing_soundboard_main.py
@@ -1,6 +1,5 @@
 import argparse
-from train_model import TrainModel
-from gesture_classification import GestureClassifier
+from training_files.gesture_classification import GestureClassifier
 
 def main():
     args = get_args()
@@ -12,24 +11,26 @@ def main():
         train(args)
     if args.use:
         use(args)
-    
+
+# Main function for the gesture recognition application
 def get_args():
     parser = argparse.ArgumentParser(description="Train and run a custom MediaPipe gesture classification model.")
     parser.add_argument('-t', '--train', action="store_true", help="Train a custom model with specified training data.")
     parser.add_argument('-u', '--use', action="store_true", help="Use a trained model on live webcam data.")
     parser.add_argument('-d', '--data-path', type=str, nargs='?', help="Specify the path for the training data folder.")
     parser.add_argument('-m', '--model-path', type=str, nargs='?', help="Specify the path for the model file.")
-    parser.print_help()
-
     return parser.parse_args()
 
+# Training class for gesture recognition
 def train(args):
+    from model_files.train_model import TrainModel
     trainer = TrainModel(args.data_path, args.model_path)
     trainer.load_data(0.8)
     trainer.train_model()
     loss, acc = trainer.evaluate_performance()
     print(f"accuracy: {acc}, loss: {loss}")
 
+# Usage class for gesture recognition
 def use(args):
     classifier = GestureClassifier(args.model_path, 0.3)
     classifier.classify_live_footage(30)
